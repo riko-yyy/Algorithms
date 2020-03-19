916f4b60d1facde3eadd6f674d42d92095856fc7
riko <c.c.3yegk1hass.w1s0vhd@gmail.com>
2020-03-19 22:36:38 +0900

    first commit

---
+++ Algorithms/KStringClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Algorithms
+{
+    public class KStringClass
+    {
+        public KStringClass()
+        {
+        }
+    }
+}
---
+++ Algorithms/TowerOfHanoiClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Algorithms
+{
+    public class TowerOfHanoiClass
+    {
+        public TowerOfHanoiClass()
+        {
+        }
+    }
+}
