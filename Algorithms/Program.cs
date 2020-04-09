41588a43df36d78bbc095ddf37ceca5d1dca498e
riko <c.c.3yegk1hass.w1s0vhd@gmail.com>
2020-04-09 20:46:19 +0900

    Add FindMax in BinaryTree and Correct comment

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -12,21 +12,21 @@ namespace Algorithms
             //Recursuve
 
             Console.WriteLine("=================================================");
-            Console.WriteLine("1)Tower of Hanoi");
+            Console.WriteLine("1-1)Tower of Hanoi");
             Console.WriteLine("-------------------------------------------------");
             TowerOfHanoiClass exe1 = new TowerOfHanoiClass();
             exe1.Execute(4, 'A', 'B', 'C');
             Console.WriteLine("=================================================");
 
             Console.WriteLine("=================================================");
-            Console.WriteLine("2)Binary");
+            Console.WriteLine("1-2)Binary");
             Console.WriteLine("-------------------------------------------------");
             BinaryClass exe2 = new BinaryClass(4);
             exe2.Execute(4);
             Console.WriteLine("=================================================");
 
             Console.WriteLine("=================================================");
-            Console.WriteLine("3)KString");
+            Console.WriteLine("1-3)KString");
             Console.WriteLine("-------------------------------------------------");
             KStringClass exe3 = new KStringClass(4);
             exe3.Execute(4, 5);
@@ -36,14 +36,14 @@ namespace Algorithms
             //Stack
 
             Console.WriteLine("=================================================");
-            Console.WriteLine("4)Parser");
+            Console.WriteLine("2-1)Parser");
             Console.WriteLine("-------------------------------------------------");
             ParserClass exe4 = new ParserClass();
             exe4.Execute("()()()()(((())))");
             Console.WriteLine("=================================================");
 
             Console.WriteLine("=================================================");
-            Console.WriteLine("5)FindSpan");
+            Console.WriteLine("2-2)FindSpan");
             Console.WriteLine("-------------------------------------------------");
             FindSpanClass exe5 = new FindSpanClass();
             exe5.Execute(new int[] { 6, 3, 4, 5, 2 }, 5);
@@ -53,7 +53,7 @@ namespace Algorithms
             //Queue
 
             Console.WriteLine("=================================================");
-            Console.WriteLine("6)SlideWindow");
+            Console.WriteLine("3-1)SlideWindow");
             Console.WriteLine("-------------------------------------------------");
             SlidingWindowClass exe6 = new SlidingWindowClass();
             exe6.Execute(new int[] { 1, 3, -1, -3, 5, 3, 6, 7 }, 3);
@@ -64,32 +64,44 @@ namespace Algorithms
 
             BinaryTreeNode root = new BinaryTreeNode(1, new BinaryTreeNode(2, new BinaryTreeNode(4, null, null), new BinaryTreeNode(5, null, null)), new BinaryTreeNode(3, null, null));
             Console.WriteLine("=================================================");
-            Console.WriteLine("7)PreOrder");
+            Console.WriteLine("4-1-1)PreOrder");
             Console.WriteLine("-------------------------------------------------");
             PreOrderClass exe7 = new PreOrderClass();
             exe7.Execute(root);
             Console.WriteLine("=================================================");
 
             Console.WriteLine("=================================================");
-            Console.WriteLine("8)InOrder");
+            Console.WriteLine("4-1-2)InOrder");
             Console.WriteLine("-------------------------------------------------");
             InOrderClass exe8 = new InOrderClass();
             exe8.Execute(root);
             Console.WriteLine("=================================================");
 
             Console.WriteLine("=================================================");
-            Console.WriteLine("9)PostOrder");
+            Console.WriteLine("4-1-3)PostOrder");
             Console.WriteLine("-------------------------------------------------");
             PostOrderClass exe9 = new PostOrderClass();
             exe9.Execute(root);
             Console.WriteLine("=================================================");
 
             Console.WriteLine("=================================================");
-            Console.WriteLine("10)LevelOrder");
+            Console.WriteLine("4-1-4)LevelOrder");
             Console.WriteLine("-------------------------------------------------");
             LevelOrderClass exe10 = new LevelOrderClass();
             exe10.Execute(root);
             Console.WriteLine("=================================================");
+
+
+            //Binary Tree App
+
+            Console.WriteLine("=================================================");
+            Console.WriteLine("4-2)FindMax");
+            Console.WriteLine("-------------------------------------------------");
+            BinaryTreeRepositoryClass repo = new BinaryTreeRepositoryClass();
+            Console.WriteLine(repo.FindMax(root));
+            Console.WriteLine("=================================================");
+
+
 
         }
     }
---
+++ Algorithms/Structure/BinaryTreeRepositoryClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Structure
+{
+    public class BinaryTreeRepositoryClass
+    {
+        public BinaryTreeRepositoryClass()
+        {
+        }
+
+
+        public int? FindMax(BinaryTreeNode root)
+        {
+            int? max = null;
+            List<int?> holder = new List<int?>();
+            if (root != null)
+            {
+                holder.Add(root.Data);
+
+                if (root.Left != null)
+                {
+                    holder.Add(FindMax(root.Left));
+                }
+
+                if (root.Right != null)
+                {
+                    holder.Add(FindMax(root.Right));
+                }
+
+                max = holder.Max();
+                
+            } 
+            return max;
+        }
+
+
+    }
+}
