7ede2f5f2d6db3c8f48d8a672352d843d7649c16
riko <c.c.3yegk1hass.w1s0vhd@gmail.com>
2020-04-12 23:29:18 +0900

    Add GCD

---
+++ Algorithms/GcdClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Algorithms
+{
+    public class GcdClass
+    {
+        public GcdClass()
+        {
+        }
+
+        /// <summary>
+        /// 最大公約数を返す
+        /// </summary>
+        /// <param name="a">整数1</param>
+        /// <param name="b">整数2</param>
+        /// <returns>最大公約数</returns>
+        public static int Gcd(int a, int b)
+        {
+            if (a < b)
+                // 引数を入替えて自分を呼び出す
+                return Gcd(b, a);
+            while (b != 0)
+            {
+                var remainder = a % b;
+                a = b;
+                b = remainder;
+            }
+            return a;
+        }
+    }
+}
